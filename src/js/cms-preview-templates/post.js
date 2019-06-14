f85f9fdf65088bee8c1b83ac17b1581c39bec52b
Glauber Ramos <glauberamos@gmail.com>
2019-06-14 19:57:44 -0300

    Small changes on translation

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -14,7 +14,7 @@ export default class PostPreview extends React.Component {
           height: "80px"
         }}></div>
         <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
-        <p>Read in x minutes</p>
+        <p>Leia em x minutos</p>
       </div>
       <div className="cms mw6">
         <p>{ entry.getIn(["data", "description"]) }</p>
